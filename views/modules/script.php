55be4adaeed29b757de760121cb7de025778565a
laortizt <adri26.ortiz@gmail.com>
2021-03-13 02:59:22 -0500

    asistencia front

--- views/modules/script.php
+++ views/modules/script.php
@@ -1,7 +1,13 @@
 <!-- Libs -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.2/angular.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <!-- Page level custom scripts -->
 <script src="<?php echo SERVERURL; ?>assets/sweet-alert/sweetalert2.min.js"></script>
 <script src="<?php echo SERVERURL; ?>assets/script/main.js"></script>
 <script src="<?php echo SERVERURL; ?>assets/script/calendar.js"></script>
+<script src="<?php echo SERVERURL; ?>assets/script/schedule.js"></script>
